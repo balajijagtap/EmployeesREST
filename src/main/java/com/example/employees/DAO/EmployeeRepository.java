9fd32c6966101517e560a718e0e03e9447f8a91e
balajijagtap <balajijagtap336@gmail.com>
2023-10-27 00:29:20 +0530

    adding dependency for spring data-jpa and rest to reduce the boiler code

--- src/main/java/com/example/employees/DAO/EmployeeRepository.java
+++ src/main/java/com/example/employees/DAO/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.example.employees.DAO;
 
 import com.example.employees.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+// to change the default path of endpoint use this annotation
+@RepositoryRestResource(path="employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // using spring data jpa
 }
